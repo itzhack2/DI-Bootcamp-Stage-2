1a0f9cd0c2218b1a5aa38bb0e14a391da7c689db
itzhack2 <143926615+itzhack2@users.noreply.github.com>
2023-12-01 23:19:11 +0200

    save

--- Week3-Javascript/Week3day4/index.js
+++ Week3-Javascript/Week3day4/index.js
@@ -68,22 +68,22 @@
 // }
 
 
-Daily Challenge: Not Bad
-const sentence= " The movie is not that bad , I like it "
-const wordNot = sentence.search("not")
-const wordBad = sentence.search("bad")
-if (wordNot== -1){
-    console.log(sentence);
-}else if ( wordNot < wordBad ){
-console.log('wordBad:',wordBad);
-console.log('wordNot:',wordNot);
+// Daily Challenge: Not Bad
+// const sentence= " The movie is not that bad , I like it "
+// const wordNot = sentence.search("not")
+// const wordBad = sentence.search("bad")
+// if (wordNot== -1){
+//     console.log(sentence);
+// }else if ( wordNot < wordBad ){
+// console.log('wordBad:',wordBad);
+// console.log('wordNot:',wordNot);
 
-const firstPart  = sentence.slice(0,wordNot)
-const secondPart = sentence.slice(wordBad + 3)
-console.log( firstPart + "good" + secondPart);
-}else{
-    console.log(sentence);
-}
+// const firstPart  = sentence.slice(0,wordNot)
+// const secondPart = sentence.slice(wordBad + 3)
+// console.log( firstPart + "good" + secondPart);
+// }else{
+//     console.log(sentence);
+// }
 
 // console.log “no one is online”.
 // console.log “<name_user> is online”.
---
+++ Week3-Javascript/Week3day5/index.js
@@ -0,0 +1,6 @@
+// Exercise 1: Your Favorite Food
+for (let i = 0; i < colors.length; i++ ){
+    console.log(colors[i]);
+}
+let x = 2;
+console.log(x % 2);
