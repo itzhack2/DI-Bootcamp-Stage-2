d4ebfe4f2fd140a54eecb27bc82f705709c8d537
itzhack2 <143926615+itzhack2@users.noreply.github.com>
2024-02-01 10:12:15 +0200

    new-commit

--- Week12-Review Async JS & Node.js (1.2)/week12day1/script.js
+++ Week12-Review Async JS & Node.js (1.2)/week12day1/script.js
@@ -1,40 +1,138 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const fetchDataButton = document.getElementById('fetchData');
-    const characterInfoContainer = document.getElementById('characterInfo');
-    const loadingMessage = document.getElementById('loading');
-    const errorMessage = document.getElementById('error');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const fetchDataButton = document.getElementById('fetchData');
+//     const characterInfoContainer = document.getElementById('characterInfo');
+//     const loadingMessage = document.getElementById('loading');
+//     const errorMessage = document.getElementById('error');
 
-    fetchDataButton.addEventListener('click', fetchRandomCharacter);
+//     fetchDataButton.addEventListener('click', fetchRandomCharacter);
 
-    function fetchRandomCharacter() {
-        loadingMessage.style.display = 'block';
-        errorMessage.style.display = 'none';
-        characterInfoContainer.innerHTML = ''; // Clear previous character info
+//     function fetchRandomCharacter() {
+//         loadingMessage.style.display = 'block';
+//         errorMessage.style.display = 'none';
+//         characterInfoContainer.innerHTML = ''; // Clear previous character info
 
-        // Make AJAX request to SWAPI
-        const apiUrl = 'https://www.swapi.tech/api/people/';
-        const randomCharacterId = Math.floor(Math.random() * 83) + 1;
+//         // Make AJAX request to SWAPI
+//         const apiUrl = 'https://www.swapi.tech/api/people/';
+//         const randomCharacterId = Math.floor(Math.random() * 83) + 1;
 
-        fetch(apiUrl + randomCharacterId)
-            .then(response => response.json())
-            .then(data => {
-                loadingMessage.style.display = 'none';
-                displayCharacterInfo(data.result.properties);
-            })
-            .catch(error => {
-                loadingMessage.style.display = 'none';
-                errorMessage.style.display = 'block';
-            });
+//         fetch(apiUrl + randomCharacterId)
+//             .then(response => response.json())
+//             .then(data => {
+//                 loadingMessage.style.display = 'none';
+//                 displayCharacterInfo(data.result.properties);
+//             })
+//             .catch(error => {
+//                 loadingMessage.style.display = 'none';
+//                 errorMessage.style.display = 'block';
+//             });
+//     }
+
+//     function displayCharacterInfo(character) {
+//         const infoHTML = `
+//             <h2>${character.name}</h2>
+//             <p>Height: ${character.height} cm</p>
+//             <p>Gender: ${character.gender}</p>
+//             <p>Birth Year: ${character.birth_year}</p>
+//             <p>Home World: ${character.homeworld}</p>
+//         `;
+//         characterInfoContainer.innerHTML = infoHTML;
+//     }
+// });
+let xhr = new XMLHttpRequest();
+
+//Select elements in DOM
+let button = document.querySelector('#button');
+let names = document.querySelector('#name');
+let height = document.querySelector('#height');
+let gender = document.querySelector('#gender');
+let birthYear = document.querySelector('#birth-year');
+let homeWorld = document.querySelector('#home-world');
+
+//Get the info from API and catch for Errors
+function getInfo (){
+  //Call Loading Data
+ 	updateWithLoading();
+  //Get Random people in the API between 1 and 88
+ 	let randomNumber = Math.floor((Math.random() * 88) + 1);
+   let apiUrl = 'https://swapi.dev/api/people/' + randomNumber + '/';
+   
+  // ajax
+  xhr.open('GET', apiUrl);
+  xhr.responseType = 'json';
+  xhr.send();
+  xhr.onload = function(){
+    if(xhr.status != 200){
+      //console.log(`Error: ${xhr.status}: ${xht.statusText}`);
+      updateInfoWithError();
+      console.log('there was an error');
     }
-
-    function displayCharacterInfo(character) {
-        const infoHTML = `
-            <h2>${character.name}</h2>
-            <p>Height: ${character.height} cm</p>
-            <p>Gender: ${character.gender}</p>
-            <p>Birth Year: ${character.birth_year}</p>
-            <p>Home World: ${character.homeworld}</p>
-        `;
-        characterInfoContainer.innerHTML = infoHTML;
+    else{
+      updateInfo(xhr.response);
     }
-});
+  }
+  xhr.onerror = function() {
+    updateInfoWithError();
+    console.log('there was an error');
+  };
+ }
+
+ //Display info on screen
+ function updateInfo(resp){
+   
+  // ajax
+  let url = new URL(resp.homeworld);
+  url.protocol = 'https:'
+  xhr.open('GET', url.href);
+
+  xhr.responseType = 'json';
+  xhr.send();
+  xhr.onload = function(){
+    if(xhr.status != 200){
+      console.log('there was an error 2');
+    }
+    else{
+      updateInfo2(xhr.response)
+    }
+  }
+  xhr.onerror = function() {
+    console.log('there was an error 3');
+  };
+ 	console.log(resp.homeworld);
+  console.log(resp.name);
+ 	console.log(names);
+
+  names.innerText = resp.name;
+  height.innerText = `Height: ${resp.height}`;
+  gender.innerText = `Gender: ${resp.gender}`;
+  birthYear.innerText = `Birth Year: ${resp.birth_year}`;
+  // homeWorld.innerText = `Home World: ${planet}`;
+}
+
+
+//Display Home World
+function updateInfo2(re){
+ 	// console.log(re.name)
+ 	// return re.name;
+ 	homeWorld.innerText = `Home World: ${re.name}`;
+}
+
+//Display when Error
+function updateInfoWithError(){
+  names.innerText = 'Oh No! That person isnt available.';
+  height.innerText = ''
+  gender.innerText = ''
+  birthYear.innerText = ''
+  homeWorld.innerText = ''
+}
+
+//Display when updating info (pending data)
+function updateWithLoading(){
+  //Icon link: https://fontawesome.com/how-to-use/on-the-web/styling/animating-icons
+  names.innerHTML = '<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i> <p>Loading...</p>';
+  height.innerText = ''
+  gender.innerText = ''
+  birthYear.innerText = ''
+  homeWorld.innerText = ''
+}
+
+button.addEventListener('click', getInfo);
